a3fe30453606f01d016fc33b4680a706a9a08945
Serhii Londar <serhii.londar@gmail.com>
2017-09-26 22:23:31 +0300

    Update to swift 4

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
+//let package = Package(
+//    name: "ScannerUtils"
+//)
 let package = Package(
-    name: "ScannerUtils"
+    name: "ScannerUtils",
+    products: [
+        .library(name: "ScannerUtils", targets: ["ScannerUtils"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "ScannerUtils", dependencies: [], path: "Sources")
+    ]
 )
