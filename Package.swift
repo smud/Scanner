8dc1cd18c0f946acaadc8d34fc6d65be26530d25
Andrey Fidrya <af@zabiyaka.com>
2018-01-21 03:36:14 +0300

    Update Package.swift to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "ScannerUtils"
+    name: "ScannerUtils",
+    products: [
+        .library(name: "ScannerUtils", targets: ["ScannerUtils"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "ScannerUtils"),
+    ]
 )
